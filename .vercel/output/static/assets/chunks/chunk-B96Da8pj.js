3ad3678ab1b6ab9ba6c652a8fbb8ee204e0ee102
Ilya Olovyannikov <excaliburgyro@gmail.com>
2024-12-12 17:56:47 +0300

    fix(single): fix single questions fields

---
+++ .vercel/output/static/assets/chunks/chunk-B96Da8pj.js
@@ -0,0 +1,1 @@
+const t="_input_1ss6d_1",n={input:t};export{n as s};
---
+++ .vercel/output/static/assets/chunks/chunk-baqAD0T3.js
@@ -0,0 +1,1 @@
+const n="_header_8zjda_1",o="_inner_8zjda_7",_="_logo_8zjda_19",e="_link_8zjda_26",a={header:n,inner:o,logo:_,link:e};export{a as s};
